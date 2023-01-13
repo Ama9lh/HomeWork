1eb09ca8404aad19820398940fc915b2a62381b4
Denis <kummerd65@gmail.com>
2023-01-13 17:14:35 +0600

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (n > m) max = n;
+if (n < m) max = m;
+
+Console.Write($"Ваше число: {max}");
