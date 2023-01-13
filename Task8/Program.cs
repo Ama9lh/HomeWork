42497db713d1242f6fed1230b50b7d21c8e57401
Denis <kummerd65@gmail.com>
2023-01-13 18:53:42 +0600

    add Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a * (0);
+while (b <= a)
+{
+    Console.Write($"{b} ");
+    b = b + 2;
+}
+
+// Я к сожалению так и не понял как сделать так, чтобы вывод начинался с единицы, а не с нуля.
