06a012a692fb13235bd0267ab540f9de3e51adb3
Denis <kummerd65@gmail.com>
2023-01-13 18:21:44 +0600

    add Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите свое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0) Console.Write("Да");
+else
+    {
+        Console.Write("Нет");
+    }
